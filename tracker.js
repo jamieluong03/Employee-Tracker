dffab44dc8a4e75ad540b881f9b9b0a74c7e1f94
Jamie Luong <jamieluong03@gmail.com>
2019-10-24 21:48:10 -0700

    added dotenv

--- tracker.js
+++ tracker.js
@@ -1,15 +1,18 @@
 // dependencies
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+require('dotenv').config();
+
 
 // create mysql connection
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 3306,
-    user: "root",
-    password: "Hellojamie3!",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "employeeTracker_db"
 });
+
 
 var roles;
 // initiate mysql connection
